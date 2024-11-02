a2d8797415d3ad41f47c2dec61e6f684397561b4
JanDwight <103998322+JanDwight@users.noreply.github.com>
2024-11-02 20:51:51 +0800

    Login

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const dotenv = require('dotenv').config()
+const {errorHandler} = require('./middleware/errMiddleWare')
+const connectDB = require('./config/db')
+const port = process.env.PORT || 5000
+
+connectDB()
+
+const app = express()
+
+// Middleware
+// This is to recognize incoming request data as a JSON object.
+app.use(express.json())
+// This one expects incoming request data to be encoded in the URL.
+app.use(express.urlencoded({ extended: false}))
+
+//Declare the path to the API/routes here
+app.use('/api/auth', require('./routes/userRoutes'));
+
+app.use(errorHandler)
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
