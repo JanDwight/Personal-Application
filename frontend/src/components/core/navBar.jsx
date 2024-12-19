b118849ce589dfd479a44fafa04054b96d68981d
JanDwight <103998322+JanDwight@users.noreply.github.com>
2024-12-19 15:04:05 +0800

    NavBar

--- frontend/src/components/core/navBar.jsx
+++ frontend/src/components/core/navBar.jsx
@@ -1,11 +1,12 @@
+import SideBar from "./sideBar";
 import UserProfile from "./userProfile";
 
 export default function NavBar() {
   return (
     <div className="flex bg-gray-500 h-fit min-w-fit justify-between items-end">
       <div className='flex justify-between w-1/4 min-w-fit ml-20'>
-        <button>Menu 1</button>
-        <button>Menu 2</button>
+        <SideBar />
+        <button>Home</button>
         <button>Menu 3</button>
       </div>
 
