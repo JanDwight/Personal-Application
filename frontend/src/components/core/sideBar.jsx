05561068842511bd1c4dbd2a23380c358411c484
JanDwight <103998322+JanDwight@users.noreply.github.com>
2024-12-19 15:04:29 +0800

    SideBar

--- frontend/src/components/core/sideBar.jsx
+++ frontend/src/components/core/sideBar.jsx
@@ -1,10 +1,37 @@
+import { useState } from "react"
+import { Bars3Icon } from "@heroicons/react/24/solid";
+
 export default function SideBar() {
+  const [isClicked, setIsClicked] = useState(false);
+
   return (
     <>
-      <div className="flex flex-col h-screen w-56 bg-gray-500">
-        <button>Menu 1</button>
-        <button>Menu 1</button>
-        <button>Menu 1</button>
+      <div>
+        <button
+          className="h-10 w-10 cursor-pointer"
+          onClick={() => setIsClicked(!isClicked)}
+        > 
+          <Bars3Icon className={`${isClicked ? 'text-white' : 'text-black'}`}/>
+        </button>
+
+        {isClicked && (
+          <div className="absolute left-0 h-dvh overflow-y-hidden w-48 bg-gray-500 shadow-lg z-10" style={{ height: "calc(100dvh - 2.9rem)" }}>
+            <ul className="py-1 text-black">
+              <li 
+                className="px-4 py-2 hover:bg-gray-100 cursor-pointer"
+                onClick={() => alert('Menu 1 Clicked')}
+              >
+                Menu 1
+              </li>
+              <li 
+                className="px-4 py-2 hover:bg-gray-100 cursor-pointer"
+                onClick={() => alert('Menu 2 Clicked')}
+              >
+                Menu 2
+              </li>
+            </ul>
+          </div>
+        )}
       </div>
     </>
   )
